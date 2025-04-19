3c650289bb8d7ae2806e0c53046d92cef435a857
Ethan Enger <engerethan@gmail.com>
2025-04-18 23:30:31 -0600

    Animations and movement

---
+++ Player/player.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+const ACCELERATION = 500
+const MAX_SPEED = 100
+const FRICTION = 400
+
+@onready var animationPlayer = $AnimationPlayer
+@onready var animationTree = $AnimationTree
+@onready var animationState = animationTree.get("parameters/playback")
+
+func _physics_process(delta: float):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength('ui_right') - Input.get_action_strength('ui_left')
+	input_vector.y = Input.get_action_strength('ui_down') - Input.get_action_strength('ui_up')
+	input_vector.normalized()
+	if(input_vector != Vector2.ZERO):
+		animationTree.set('parameters/Idle/blend_position', input_vector)
+		animationTree.set('parameters/Run/blend_position', input_vector)
+		animationState.travel('Run')
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		animationState.travel('Idle')
+		velocity = velocity.move_toward(Vector2.ZERO, delta * FRICTION)
+	
+	print(move_and_slide())
+	
